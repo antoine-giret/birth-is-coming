73bdfcd20c082fd583a6b5050ace00303d8dd710
Antoine Giret <antoine.giret@geovelo.fr>
2024-12-08 09:33:39 +0100

    chore: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+
+import './globals.css';
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+});
+
+export const metadata: Metadata = {
+  title: 'Bébé arrive',
+  description: '🚀 Lancez les paris sur la naissance de votre futur bébé.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html className="h-full bg-gray-100" lang="fr">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}>
+        {children}
+      </body>
+    </html>
+  );
+}
